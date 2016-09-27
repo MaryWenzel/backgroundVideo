345cff830e335447bfba9013ed7a121ed0d2e12e
Sam Linnett <linnettsam@gmail.com>
2016-09-27 19:05:39 +0100

    2.0.0: Continuation of adapting backgroundVideo to plain JavaScript

--- src/backgroundVideo.js
+++ src/backgroundVideo.js
@@ -27,7 +27,9 @@
     parallax: {
       effect: 1.5
     },
-    pauseVideoOnViewLoss: false
+    pauseVideoOnViewLoss: false,
+    preventContextMenu: false,
+    minimumVideoWidth: 400
   };
 
   /**
@@ -39,15 +41,6 @@
     }
     else {
       el.className += ' ' + className;
-    }
-  };
-
-  const removeClass = function (el, className) {
-    if (el.classList) {
-      el.classList.remove(className);
-    }
-    else {
-      el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
     }
   };
 
@@ -76,7 +69,7 @@
       // Loop through each video and init
 
       for(let i = 0; i < this.element.length; i++) {
-        this.init(this.element[i]);
+        this.init(this.element[i], i);
       }
     }
 
@@ -85,18 +78,145 @@
      *
      * @method init
      * @params element
+     * @params {number} iteration
      */
-    init(element) {
+    init(element, iteration) {
       this.el = element;
+      this.playEvent = this.videoReadyCallback.bind(this);
 
-      this.setVideoWrap();
+      this.setVideoWrap(iteration);
       this.setVideoProperties()
       this.insertVideos();
 
+      // Add event listener to detect when the video can play through
+      this.el.addEventListener('canplaythrough', this.playEvent, false);
+      // If video is cached, the video will already be ready so
+      // canplay/canplaythrough event will not fire.
+      if (this.el.readyState > 3) {
+        this.videoReadyCallback();
+      };
+
+      // Prevent context menu on right click for object
+      if (this.options.preventContextMenu) {
+        // this.el.addEventListener('contextmenu', () => return false);
+      }
     }
 
-    setVideoWrap() {
+    /**
+     * Function is triggered when the video is ready to be played
+     *
+     * @method videoReadyCallback
+     */
+    videoReadyCallback() {
+      // Prevent event from being repeatedly called
+      this.el.removeEventListener('canplaythrough', this.playEvent, false);
+
+      // Set original video height and width for resize and initial calculations
+      this.options.originalVideoW = this.el.videoWidth;
+      this.options.originalVideoH = this.el.videoWidth;
+
+      // Bind events for scroll, reize and parallax
+      this.bindEvents();
+      // Request first tick
+      this.requestTick();
+    }
+
+    bindEvents() {
+      this.ticking = false;
+
+      if (this.options.parallax) {
+        window.addEventListener('scroll', this.requestTick.bind(this));
+      }
+
+      window.addEventListener('resize', this.requestTick.bind(this));
+    }
+
+    updatePosition() {
+      this.positionObject();
+      this.ticking = false;
+    }
+
+    requestTick() {
+      if (!this.ticking) {
+        window.requestAnimationFrame(this.updatePosition.bind(this));
+        this.ticking = true;
+      }
+    }
+
+    positionObject() {
+      const scrollPos = window.pageYOffset;
+      let {xPos, yPos} = this.scaleObject();
+
+      // Check for parallax
+      if (this.options.parallax) {
+        // Prevent parallax when scroll position is negative to the window
+        if (scrollPos >= 0) {
+          yPos = this.calculateYPos(yPos, scrollPos);
+        } else {
+          yPos = this.calculateYPos(yPos, 0);
+        }
+      } else {
+        yPos = -yPos;
+      }
+
+      const transformStyle = (this.options.has3d) ? `translate3d(-${xPos}px, ${yPos}px, 0)` : `translate(-${xPos}px, ${yPos}px)`;
+      // Style with prefix
+      this.el.style[`${this.options.browserPrexix}transform`] = transformStyle;
+      // Style without prefix
+      this.el.style.transform = transformStyle;
+    }
+
+    scaleObject() {
+      const windowWidth = window.innerWidth;
+      const windowHeight = window.innerHeight;
+      const heightScale = windowWidth / this.options.originalVideoW;
+      const widthScale = windowHeight / this.options.originalVideoH;
+      let scaleFactor;
+
+      this.options.bvVideoWrap.style.width = `${windowWidth}px`;
+      this.options.bvVideoWrap.style.height = `${windowHeight}px`;
+
+      scaleFactor = heightScale > widthScale ? heightScale : widthScale;
+
+      if (scaleFactor * this.options.originalVideoW < this.options.minimumVideoWidth) {
+       scaleFactor = this.options.minimumVideoWidth / this.options.originalVideoW;
+      }
+
+      const videoWidth = scaleFactor * this.options.originalVideoW;
+      const videoHeight = scaleFactor * this.options.originalVideoH;
+
+      this.el.style.width = `${videoWidth}px`;
+      this.el.style.height = `${videoHeight}px`;
+
+      return {
+       // Return x and y axis values for positioning
+       xPos: -(parseInt(this.el.style.width - windowWidth / 2)),
+       yPos: parseInt(this.el.style.height - windowHeight) / 2
+      };
+    }
+
+    calculateYPos(yPos, scrollPos) {
+      const videoPosition = parseInt(this.options.bvVideoWrap.offsetTop);
+      const videoOffset = videoPosition - scrollPos;
+      console.log(scrollPos);
+      yPos = -((videoOffset / this.options.parallax.effect) + yPos);
+
+      return yPos;
+    }
+
+    /**
+     * Create a container around the video tag
+     *
+     * @method setVideoWrap
+     */
+    setVideoWrap(iteration) {
       const wrapper = document.createElement('div');
+
+      // Set video wrap class for later use in calculations
+      this.options.bvVideoWrapClass = `bv-video-wrap-${iteration}`;
+
+      addClass(wrapper, 'bv-video-wrap');
+      addClass(wrapper, this.options.bvVideoWrapClass);
 
       wrapper.style.position = 'relative';
       wrapper.style.overflow = 'hidden';
@@ -104,8 +224,16 @@
 
       this.el.parentNode.insertBefore(wrapper, this.el);
       wrapper.appendChild(this.el);
+
+      // Set wrapper element for class wide use
+      this.options.bvVideoWrap = document.querySelector(`.${this.options.bvVideoWrapClass}`);
     }
 
+    /**
+     * Set attributes and styles for video
+     *
+     * @method setVideoProperties
+     */
     setVideoProperties() {
       this.el.setAttribute('preload', 'metadata');
       this.el.setAttribute('loop', 'true');
@@ -114,8 +242,12 @@
       this.el.style.zIndex = '1';
     }
 
+    /**
+     * Insert videos from `src` property defined
+     *
+     * @method insertVideos
+     */
     insertVideos() {
-      // Add video sources
       for(let i = 0; i < this.options.src.length; i++) {
         let videoTypeArr = this.options.src[i].split('.');
         let videoType = videoTypeArr[videoTypeArr.length - 1];
@@ -124,6 +256,13 @@
       }
     }
 
+    /**
+     * Insert videos from `src` property defined
+     *
+     * @method insertVideos
+     * @params {string} src - source of the video
+     * @params {string} type - type of video
+     */
     addSourceToVideo(src, type) {
       const source = document.createElement('source');
 
@@ -133,6 +272,11 @@
       this.el.appendChild(source);
     }
 
+    /**
+     * Detect browser and return browser prefix for CSS
+     *
+     * @method detectBrowser
+     */
     detectBrowser() {
       const val = navigator.userAgent.toLowerCase();
       let browserPrexix;
@@ -150,6 +294,11 @@
       return browserPrexix;
     }
 
+    /**
+     * Shim requestAnimationFrame to ensure it is available to all browsers
+     *
+     * @method shimRequestAnimationFrame
+     */
     shimRequestAnimationFrame() {
       /* Paul Irish rAF.js: https://gist.github.com/paulirish/1579671 */
       var lastTime = 0;
@@ -176,6 +325,11 @@
         };
     }
 
+    /**
+     * Detect if 3D transforms are avilable in the browser
+     *
+     * @method detect3d
+     */
     detect3d() {
       var el = document.createElement('p'),
         t, has3d,
